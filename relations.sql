b98afb606cc1eb7e18abdff70d4db4b6890ee00a
lovernan <1796501485@qq.com>
2018-09-19 15:45:41 +0800

    third

---
+++ relations.sql
@@ -0,0 +1,5 @@
+INSERT INTO `relations` VALUES (1, 3, 1);
+INSERT INTO `relations` VALUES (2, 3, 2);
+INSERT INTO `relations` VALUES (4, 1, 1);
+INSERT INTO `relations` VALUES (5, 1, 3);
+INSERT INTO `relations` VALUES (7, 3, 6);
---
+++ user.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` VALUES (1, '向死而生', '谷歌', '我就是我', 123213214, 'http://www.gx8899.com/uploads/allimg/2016062221/2425gvasjz2.jpg', '经理');
+INSERT INTO `user` VALUES (2, '璀错', '腾讯', '生如夏花，脚踏实地', 314234214, 'http://www.gx8899.com/uploads/allimg/2017092012/0c353duwsjl-lp.jpg', '主席');
+INSERT INTO `user` VALUES (3, '幸福着你的幸福', NULL, NULL, NULL, 'https://wx.qlogo.cn/mmopen/vi_32/2Wn9yHJT3F9YAmOYUBrvtFDqblHej16EvzMt5mVswnUZRGbZOVm4Eia8RibNRm6ibuqcxn01zhenibdoro9Q5QGOcg/132', '职员');
